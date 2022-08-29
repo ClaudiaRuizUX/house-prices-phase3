754d86d231ef652b62919436bed5554c73106915
claudiaruiz <claudiaruizux@gmail.com>
2022-08-29 21:40:46 +1000

    finished mvp level one

--- app/cli.rb
+++ app/cli.rb
@@ -14,7 +14,7 @@ class CLI
         learnMore = "#{@decorationOneLine}Learn more about housing:"
 
         # index home
-        instructions = "
+        instructionsOne = "
         Choose how to View Dwelling Prices & Population #{@decorationOneLine}"
         indexHome =  "
         1 In a Specific Location (5 Year Timeline)
@@ -26,7 +26,7 @@ class CLI
         puts welcome
         puts intro
         puts learnMore
-        puts instructions
+        puts instructionsOne
         puts indexHome
         input = gets.strip
 
@@ -54,60 +54,90 @@ class CLI
 
     def loadSpecificLocation
         # content specific location
-        intro = "
+        @intro = "
         #{@decorationOneLine}Prices in a Specific Location (5 Year Timeline)"
-        instructions = "
+        @instructionsTwo = "
         (enter a Location Name or Number)
         "
         @specificLocationContent = ["Australian Capital Territory", "Northern Territory", "New South Wales",
         "Queensland", "South Australia", "Tasmania", "Victoria", "Western Australia"]
-        subtitle = "Median Dwelling Prices & Population"
-
+        @subtitle = "Median Dwelling Prices & Population"
         # specificLocation section
-        puts intro
+        puts @intro
         @specificLocationContent.each_with_index {|subject, index| puts "#{@decorationOneLine} #{index+1} #{subject}" }
-        puts instructions
-    
-        # switcher
-        input = gets.strip
+        puts @instructionsTwo
+        @input = gets.strip
+        if @input.empty?
+            @input = gets.strip
+         else
+            loadSectionTwo (@input)
+         end
+         @input = gets.strip
+         loadSectionTwo (@input)
+         @input = gets.strip
+         loadSectionTwo (@input)
+         @input = gets.strip
+         loadSectionTwo (@input)
+
+         @input = gets.strip
+         loadSectionTwo (@input)
+         @input = gets.strip
+         loadSectionTwo (@input)
+         @input = gets.strip
+         loadSectionTwo (@input)
+
+         @input = gets.strip
+         loadSectionTwo (@input)
+         @input = gets.strip
+         loadSectionTwo (@input)
+         @input = gets.strip
+         loadSectionTwo (@input)
+    end
+
+    def loadSectionTwo (input)
         regionName = ""
         case input
-            when "1", "Australian Capital Territory", "ACT"
-                regionName = "ACT"
-                # test = Population.find_by(name: regionName)
-                # puts test.population
-                puts " #{@decorationOneLine} #{subtitle} in #{@specificLocationContent[0]}"
-                input = 4
-            when "2", "Northern Territory", "NT"
-                regionName = "Northern Territory"
-                puts " #{@decorationOneLine} #{subtitle} in #{@specificLocationContent[1]}"
-                input = 2
-            when "3", "New South Wales", "NSW"
-                regionName = "New South Wales"
-                puts " #{@decorationOneLine} #{subtitle} in #{@specificLocationContent[2]}"
-                input = 0
-            when "4", "Queensland"
-                puts " #{@decorationOneLine} #{subtitle} in #{@specificLocationContent[3]}"
-                input = 7
-            when "5", "South Australia"
-                puts " #{@decorationOneLine} #{subtitle} in #{@specificLocationContent[4]}"
-                input = 3
-            when "6", "Tasmania"
-                puts " #{@decorationOneLine} #{subtitle} in #{@specificLocationContent[5]}"
-                input = 1
-            when "7", "Victoria"
-                puts " #{@decorationOneLine} #{subtitle} in #{@specificLocationContent[6]}"
-                input = 6
-            when "8", "Western Australia"
-                puts " #{@decorationOneLine} #{subtitle} in #{@specificLocationContent[7]}"
-                input = 5
-            else
-                puts "sorry we that location is not available, try some from the below"
-                @specificLocationContent.each_with_index {|subject, index| puts "#{@decorationOneLine} #{index+1} #{subject}" }
-        puts instructions
+        when "1", "Australian Capital Territory", "ACT"
+            regionName = "ACT"
+            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[0]}"
+            input = 4
+        when "2", "Northern Territory", "NT"
+            regionName = "Northern Territory"
+            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[1]}"
+            input = 2
+        when "3", "New South Wales", "NSW"
+            regionName = "New South Wales"
+            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[2]}"
+            input = 0
+        when "4", "Queensland"
+            regionName = "Queensland"
+            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[3]}"
+            input = 7
+        when "5", "South Australia"
+            regionName = "South Australia"
+            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[4]}"
+            input = 3
+        when "6", "Tasmania"
+            regionName = "Tasmania"
+            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[5]}"
+            input = 1
+        when "7", "Victoria"
+            regionName = "Victoria"
+            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[6]}"
+            input = 6
+        when "8", "Western Australia"
+            regionName = "Western Australia"
+            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[7]}"
+            input = 5
+        else
+            puts "sorry we that location is not available, try some from the below"
+            @specificLocationContent.each_with_index {|subject, index| puts "#{@decorationOneLine} #{index+1} #{subject}" }
+            puts @instructionsTwo
         end
         selectedRegion = GetRegions.new
         puts selectedRegion.region_price(input, regionName)
+        puts " (type another region or hit enter to quit)
+        "
     end
 
 end
