8558e205460604ac0c18312fc0d6a9d8358f4abd
claudiaruiz <claudiaruizux@gmail.com>
2022-08-30 18:17:47 +1000

    finished MVP

--- app/cli.rb
+++ app/cli.rb
@@ -15,55 +15,22 @@ class CLI
 
         # index home
         instructionsOne = "
-        Choose how to View Dwelling Prices & Population #{@decorationOneLine}"
-        indexHome =  "
-        1 In a Specific Location (5 Year Timeline)
-        2 On All Locations in a Year
-
-        (enter 1 or 2)
-        "
-        # sectionHome
+        View a Specific Location in a 5 Year Timeline) #{@decorationOneLine}"
         puts welcome
         puts intro
         puts learnMore
         puts instructionsOne
-        puts indexHome
-        input = gets.strip
-
-        # switcher
-        if input == "1"
-            loadSpecificLocation
-        elsif input == "2"
-            puts "Data on All Locations Coming Soon"
-        else
-        # error 
-            puts "please enter either 1 or 2 instead"
-            input = gets.strip
-            begin
-                #... process, may raise an exception
-            rescue => error
-               puts error.message
-               puts "Enter 1 or 2 instead of #{input}"
-            else
-                puts "the app is going to quit now"
-            ensure
-                input = gets.strip
-            end
-        end
+        loadSpecificLocation
     end
 
     def loadSpecificLocation
-        # content specific location
-        @intro = "
-        #{@decorationOneLine}Prices in a Specific Location (5 Year Timeline)"
         @instructionsTwo = "
         (enter a Location Name or Number)
         "
         @specificLocationContent = ["Australian Capital Territory", "Northern Territory", "New South Wales",
         "Queensland", "South Australia", "Tasmania", "Victoria", "Western Australia"]
-        @subtitle = "Median Dwelling Prices & Population"
-        # specificLocation section
-        puts @intro
+        @subtitle = "Median Prices (Q1) & Population"
+
         @specificLocationContent.each_with_index {|subject, index| puts "#{@decorationOneLine} #{index+1} #{subject}" }
         puts @instructionsTwo
         @input = gets.strip
@@ -74,24 +41,26 @@ class CLI
          end
          @input = gets.strip
          loadSectionTwo (@input)
-         @input = gets.strip
-         loadSectionTwo (@input)
-         @input = gets.strip
-         loadSectionTwo (@input)
+        #  @input = gets.strip
+        #  loadSectionTwo (@input)
+        #  @input = gets.strip
+        #  loadSectionTwo (@input)
+        #  @input = gets.strip
+        #  loadSectionTwo (@input)
 
-         @input = gets.strip
-         loadSectionTwo (@input)
-         @input = gets.strip
-         loadSectionTwo (@input)
-         @input = gets.strip
-         loadSectionTwo (@input)
+        #  @input = gets.strip
+        #  loadSectionTwo (@input)
+        #  @input = gets.strip
+        #  loadSectionTwo (@input)
+        #  @input = gets.strip
+        #  loadSectionTwo (@input)
 
-         @input = gets.strip
-         loadSectionTwo (@input)
-         @input = gets.strip
-         loadSectionTwo (@input)
-         @input = gets.strip
-         loadSectionTwo (@input)
+        #  @input = gets.strip
+        #  loadSectionTwo (@input)
+        #  @input = gets.strip
+        #  loadSectionTwo (@input)
+        #  @input = gets.strip
+        #  loadSectionTwo (@input)
     end
 
     def loadSectionTwo (input)
@@ -99,35 +68,35 @@ class CLI
         case input
         when "1", "Australian Capital Territory", "ACT"
             regionName = "ACT"
-            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[0]}"
+            puts " #{@decorationOneLine} #{@specificLocationContent[0]} - #{@subtitle}"
             input = 4
         when "2", "Northern Territory", "NT"
             regionName = "Northern Territory"
-            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[1]}"
+            puts " #{@decorationOneLine} #{@specificLocationContent[1]} - #{@subtitle}"
             input = 2
         when "3", "New South Wales", "NSW"
             regionName = "New South Wales"
-            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[2]}"
+            puts " #{@decorationOneLine} #{@specificLocationContent[2]} - #{@subtitle}"
             input = 0
         when "4", "Queensland"
             regionName = "Queensland"
-            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[3]}"
+            puts " #{@decorationOneLine} #{@specificLocationContent[3]} - #{@subtitle}"
             input = 7
         when "5", "South Australia"
             regionName = "South Australia"
-            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[4]}"
+            puts " #{@decorationOneLine} #{@specificLocationContent[4]} - #{@subtitle}"
             input = 3
         when "6", "Tasmania"
             regionName = "Tasmania"
-            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[5]}"
+            puts " #{@decorationOneLine} #{@specificLocationContent[5]} - #{@subtitle}"
             input = 1
         when "7", "Victoria"
             regionName = "Victoria"
-            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[6]}"
+            puts " #{@decorationOneLine} #{@specificLocationContent[6]} - #{@subtitle}"
             input = 6
         when "8", "Western Australia"
             regionName = "Western Australia"
-            puts " #{@decorationOneLine} #{@subtitle} in #{@specificLocationContent[7]}"
+            puts " #{@decorationOneLine} #{@specificLocationContent[7]} - #{@subtitle}"
             input = 5
         else
             puts "sorry we that location is not available, try some from the below"
@@ -138,6 +107,8 @@ class CLI
         puts selectedRegion.region_price(input, regionName)
         puts " (type another region or hit enter to quit)
         "
+        @input = gets.strip
+        loadSectionTwo (@input)
     end
 
 end
